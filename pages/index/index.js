7ce424afb1f837121bde5caa3c1cb5d206fc03f6
mengxuan <357388897@qq.com>
2020-11-24 18:18:37 +0800

    wyy音乐
    利用git保存

---
+++ pages/index/index.js
@@ -0,0 +1,82 @@
+// pages/index/index.js
+import request from "../../utils/request"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      bannerlist:[],
+      recommdlist:[],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function (options) {
+    // request('http://localhost:3000/banner',{type:2})
+  let requestBanner =await request('/banner',{type:2})
+  //  console.log('请求成功：',requestBanner); 
+   this.setData({
+     bannerlist:requestBanner.banners
+   })
+  //  console.log(this.data.bannerlist,"bannerlist");
+    // 推荐歌单
+    let recommd = await request('/personalized',{limit:10})
+    console.log(recommd);
+    
+    this.setData({
+      recommdlist:recommd.result
+    })
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+ 
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
