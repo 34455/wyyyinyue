4163254a4f4685c9f3e1fcdfb00323bdb6cce7b9
mengxuan <357388897@qq.com>
2020-11-26 15:34:58 +0800

    动态添加排行榜，调试真机

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,16 @@
 // pages/index/index.js
 import request from "../../utils/request"
+
 Page({
+
 
   /**
    * 页面的初始数据
    */
   data: {
-      bannerlist:[],
-      recommdlist:[],
+    bannerlist: [],
+    recommdlist: [],
+    toplist: []
   },
 
   /**
@@ -15,18 +18,35 @@ Page({
    */
   onLoad: async function (options) {
     // 首页轮播图
-  let requestBanner =await request('/banner',{type:2})
-  //  console.log('请求成功：',requestBanner); 
-   this.setData({
-     bannerlist:requestBanner.banners
-   })
-  //  console.log(this.data.bannerlist,"bannerlist");
-    // 推荐歌单
-    let recommd = await request('/personalized',{limit:10})
+    let requestBanner = await request('/banner', { type: 2 })
+    //  console.log('请求成功：',requestBanner); 
     this.setData({
-      recommdlist:recommd.result
+      bannerlist: requestBanner.banners
     })
-    
+    //  console.log(this.data.bannerlist,"bannerlist");
+    // 推荐歌单
+    let recommd = await request('/personalized', { limit: 10 })
+    this.setData({
+      recommdlist: recommd.result
+    })
+    // 排行榜
+    let index = 0;
+    let topArr = [];
+    let name = null;
+    while (index < 5) {
+      let toplist = await request('/top/list', { idx: index++ })
+      toplist.playlist.tracks.forEach(item => {
+        item.singer = item.ar[0].name
+      });
+      topArr.push({ name: toplist.playlist.name, tracks: toplist.playlist.tracks })
+    }
+
+    this.setData({
+      toplist: topArr
+    })
+    console.log('datatoplist', this.data.toplist);
+    // let toplist = await request('/top/list', { idx: 1 })
+    // console.log(toplist);
   },
 
   /**
@@ -40,7 +60,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
- 
+
   },
 
   /**
