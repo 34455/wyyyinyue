01b3cf3d39309cd8d88cafbcc590bae09d9a8f23
mengxuan <357388897@qq.com>
2020-11-27 18:45:27 +0800

    登录页面跳转到个人中心 并显示用户相关信息

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,130 @@
 // pages/login/login.js
+
+import request from "../../utils/request"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    phone: "",
+    password: ""
   },
-
+  //获取密码和账号
+  handleinput(e) {
+    // let type = e.currentTarget.id;   //通过id 获取用户账号和密码  id单个的时候可以使用
+    let type = e.currentTarget.dataset.type   //通过data-key=value 获取用户账号和密码  获取多个的时候可以使用
+    this.setData({
+      [type]: e.detail.value
+    })
+  },
+  // 登录
+  async login() {
+    let { phone, password } = this.data;
+    // 判断手机号
+    if (!phone) {
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    let phoneRegex = (/^1[3|4|5|7|8][0-9]{9}$/)
+    if (!phoneRegex.test(phone)) {
+      wx.showToast({
+        title: '手机号格式请填写正确',
+        icon: 'none'
+      })
+      return;
+    }
+    // 判断密码
+    if (!password) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    const result = await request('/login/cellphone', { phone, password })
+    // console.log('reqlogin',rqLogin)
+    if (result.code === 200) {
+      wx.showToast({
+        title: '登录成功',
+        icon: 'none'
+      })
+      wx.setStorageSync('userinfo', JSON.stringify(result.profile))
+      wx.reLaunch({
+        url: '/pages/personl/personl',
+      })
+      return;
+    }
+    if (result.code == 400 || result.code == 502) {
+      wx.showToast({
+        title: '手机号或者密码错误',
+        icon: 'none'
+      })
+      return
+    } else {
+      wx.showToast({
+        title: `${result.message}`,
+        icon: 'none'
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
--- pages/personl/personl.js
+++ pages/personl/personl.js
@@ -1,54 +1,90 @@
 // pages/personl/personl.js
+import request from "../../utils/request";
 let startY = 0; // 手指起始的坐标
 let moveY = 0; // 手指移动的坐标
-let movedistance = 0; // 手指移动的距离
+let movedistance = 0; // 手指移动的距离 
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    covertranslate:`translateY(0)`,
-    transition:``
+    covertranslate: `translateY(0)`,
+    transition: ``,
+    userinfo:{},
+    recentPlayList:[]
   },
-// 手指触发最初位置
-  handlestart(e){
+  // 手指触发最初位置
+  handlestart(e) {
     // 获取手指最初位置
     startY = e.touches[0].clientY
     this.setData({
-      transition:``
+      transition: ``
     })
   },
   // 手指移动
-  handlemove(e){
-// 获取手指移动位置
-     moveY = e.touches[0].clientY
-     movedistance  = moveY-startY
-     if(movedistance<=0){
-       return
-     }else if(movedistance>=80) {
-     movedistance=80
-     }
-this.setData({
-  covertranslate:`translateY(${movedistance}rpx)`
-}) 
+  handlemove(e) {
+    // 获取手指移动位置
+    moveY = e.touches[0].clientY
+    movedistance = moveY - startY
+    if (movedistance <= 0) {
+      return
+    } else if (movedistance >= 80) {
+      movedistance = 80
+    }
+    this.setData({
+      covertranslate: `translateY(${movedistance}rpx)`
+    })
   },
   // 手指移动结束
-  handleend(e){
-    console.log('end',e);
+  handleend(e) {
+    // console.log('end',e);
     this.setData({
-      covertranslate:`translateY(0rpx)`,
-      transition:`transform 1s linear`
-    }) 
+      covertranslate: `translateY(0rpx)`,
+      transition: `transform 1s linear`,
+      recentPlayListData:[]
+    })
+  },
+  // 跳转到个人中心
+  toLogin() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  
-  onLoad: function (options) {
 
+  onLoad:  function (options) {
+    // 获取用户信息
+    let userinfo = wx.getStorageSync('userinfo')
+    // console.log("userinfo",userinfo); 
+    if(userinfo){
+      this.setData({
+        userinfo:JSON.parse(userinfo)
+      })
+     
+    }
+      // 获取用户播放量
+    this.playrecord( this.data.userinfo.userId)
+    // console.log(id)
   },
-
+   // 获取用户播放量
+  async playrecord(userId){
+    console.log(userId)
+  const recentPlayListData= await request('/user/record',{uid:userId});
+    console.log('playrecordlist',recentPlayListData)
+    let index =0;
+  let recentPlayList= this.recentPlayListData.weekData.slice(0,10).map((item)=>{
+      item.id=index++
+      return item
+    })
+  this.setData({
+    recentPlayList
+  })
+  
+ },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
